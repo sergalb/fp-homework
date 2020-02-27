acbe183448d5348ce242df99a298639b00f21671
serg_alb <sergey-dzr2@yandex.ru>
2020-02-27 23:58:12 +0300

    fix main

--- hw0/app/Main.hs
+++ hw0/app/Main.hs
@@ -2,8 +2,6 @@
 
 module Main where
 
-import Task5
-
 main :: IO ()
-main = putStrLn $ show $ churchToInt $ churchPlus (\f x -> f $ f $ f $ f x :: Integer) (\f x -> f $ f $ f x :: Integer)
+main = putStrLn "homework 0 done"
 
